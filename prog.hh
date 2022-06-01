fff3194946a56e8aeaf4209f963825e5044052c8
kanokkorn <kanokorn@outlook.jp>
2022-06-01 07:11:10 +0700

    move function to top

--- prog.hh
+++ prog.hh
@@ -11,8 +11,30 @@
 #include <opencv2/highgui.hpp>
 #include <spdlog/spdlog.h>
 
-cv::Mat imgproc(cv::Mat);
-int counter(cv::Mat);
+/* Solution from https://stackoverflow.com/question/8449378 */
+cv::Mat imgproc(cv::Mat input) {
+  cv::cvtColor(input, input, cv::COLOR_BGR2GRAY);
+  cv::threshold(input, input, 128, 255, cv::THRESH_BINARY);
+  std::vector<std::vector<cv::Point> > contours;
+  cv::Mat contourOutput = input.clone();
+  cv::findContours( contourOutput, contours, cv::RETR_LIST, cv::CHAIN_APPROX_NONE);
+  /* Draw contours */
+  cv::Mat contourImage(input.size(), CV_8UC3, cv::Scalar(0, 0, 0));
+  cv::Scalar colors[3];
+  colors[0] = cv::Scalar(255, 0, 0);
+  colors[1] = cv::Scalar(0, 255, 0);
+  colors[2] = cv::Scalar(0, 0, 255);
+  for (size_t idx = 0; idx < contours.size(); idx++) {
+    cv::drawContours(contourImage, contours, idx, colors[idx % 5]);
+  }
+  return contourImage;
+}
+
+/* count contours in image */
+int counter(cv::Mat contourImage) {
+  int num;
+  return 0;
+}
 
 void get_frame(uint8_t cam_id, uint8_t vid_width, uint8_t vid_height) {
   cv::VideoCapture cap(cam_id, cv::CAP_ANY);
@@ -47,27 +69,6 @@ void get_frame(char** video_file) {
   }
 }
 
-/* Solution from https://stackoverflow.com/question/8449378 */
-cv::Mat imgproc(cv::Mat input) {
-  cv::cvtColor(input, input, cv::COLOR_BGR2GRAY);
-  cv::threshold(input, input, 128, 255, cv::THRESH_BINARY);
-  std::vector<std::vector<cv::Point> > contours;
-  cv::Mat contourOutput = input.clone();
-  cv::findContours( contourOutput, contours, cv::RETR_LIST, cv::CHAIN_APPROX_NONE);
-  /* Draw contours */
-  cv::Mat contourImage(input.size(), CV_8UC3, cv::Scalar(0, 0, 0));
-  cv::Scalar colors[3];
-  colors[0] = cv::Scalar(255, 0, 0);
-  colors[1] = cv::Scalar(0, 255, 0);
-  colors[2] = cv::Scalar(0, 0, 255);
-  for (size_t idx = 0; idx < contours.size(); idx++) {
-    cv::drawContours(contourImage, contours, idx, colors[idx % 5]);
-  }
-  return contourImage;
-}
 
-int counter(cv::Mat contourImage) {
-
-}
 
 #endif /* __PROG_H__ */
