ff34ea48ca8d925d3075bf6c05f3ab18d14b1cb5
kanokkorn <kanokorn@outlook.jp>
2023-05-28 22:44:36 +0700

    using syscall write in instead of `std::cout`

--- main.cc
+++ main.cc
@@ -1,10 +1,15 @@
 /*
- * File: main.cc
- * Author: kanokkorn kornkankit <kanokorn@outlook.jp>
- * consider rewrite to be usable next merge
+ * 	File: main.cc
+ * 	Author: kanokkorn kornkankit <kanokorn@outlook.jp>
+ * 	consider rewrite to be usable next merge
  */
 
 #include "main.hh"
+
+/* error message - */
+const char* cam_error = "can't open camera\n";
+const char* config_error = "failed to load config file\n";
+const char* check_cam = "checking camera\n";
 
 /* contour and return counting number */
 cv::Mat img_counter(cv::Mat input) {
@@ -25,19 +30,14 @@ cv::Mat img_counter(cv::Mat input) {
 }
 
 /* read frame from camera input or video */
-int img_get_frame(
-    uint8_t cam_id,
-    uint8_t vid_width,
-    uint8_t vid_height
-    ) {
-  cv::VideoCapture cap(cam_id, cv::CAP_ANY);
+int img_get_frame(void) {
+  cv::VideoCapture cap(CAMERA_ID, cv::CAP_ANY);
   if (!cap.isOpened()) {
-    std::cout << "cant open camera" << std::endl;
+    write(1, cam_error, sizeof(cam_error)-1);
     return 1;
   }
-  cap.set(cv::CAP_PROP_FRAME_WIDTH, vid_width);
-  cap.set(cv::CAP_PROP_FRAME_HEIGHT,vid_height);
-#pragma omp parallel
+  cap.set(cv::CAP_PROP_FRAME_WIDTH, CAM_WIDTH);
+  cap.set(cv::CAP_PROP_FRAME_HEIGHT,CAM_HEIGHT);
   while (cap.isOpened()) {
     cv::Mat frame;
     cap >> frame;
@@ -64,15 +64,15 @@ void int_handler(int signum) {
 }
 #endif
 
-int main(void) {
-  printf("Crangon : %d.%d\n", VERSION, REVISION);
-  printf("OpenCV : %s\n", CV_VERSION);
+int main(int argc, char** argv) {
+  char *c;
+  write(1, cam_error, strlen(cam_error));
   std::ifstream ifs("config_file");
   if (ifs.fail()) {
-    perror("error loading config file");
+    write(1, config_error, strlen(config_error));
     exit(-1);
   }
-  std::cout << "checking camera..." << std::endl;
+  write(1, check_cam, strlen(check_cam));
   /* read image from camera */
   cv::Mat img_zero;
   img_zero = cv::Mat::zeros(1, 1, CV_64F);
