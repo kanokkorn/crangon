9618e0de8dc83c2e1a98b59ee392206cdcbf1f47
kanokkorn <kanokorn@outlook.jp>
2023-10-11 23:59:01 +0700

    update params

--- main.hh
+++ main.hh
@@ -10,7 +10,6 @@
 #include <cstdlib>
 #include <csignal>
 
-
 /* unix pipe for gui front-end + syscall for better stdout throughput */
 #if defined(__linux__) || defined (__gnu_linux__)
 #include <sys/types.h>
@@ -40,29 +39,34 @@
 #include <omp.h>
 #endif
 
+extern "C" {
+  void check_valid_file();
+  #include "parser.c"
+};
+
 /* pre-define camera setting based on webcam */
-#define BUF_SIZE 4096
-#define COLR_CHAN 3
-#define CAM_WIDTH 1280
-#define CAM_HEIGHT 720
-#define CAMERA_ID 0
+#define BUF_SIZE    4096
+#define COLR_CHAN   3
+#define CAM_WIDTH   1280
+#define CAM_HEIGHT  720
+#define CAMERA_ID   0
 #define CAMERA_PATH "/dev/video"
 
 /* fixed resolution cam, no need for dynamic allocation */
 typedef struct {
-  uint8_t vid_id;
-  uint16_t vid_width;
-  uint16_t vid_height;
+  uint8_t     vid_id;
+  uint16_t    vid_width;
+  uint16_t    vid_height;
   std::string vid_path;
 }vid_conf;
 
 /* output data struct */
 typedef struct {
-  uint32_t real_out;
-  uint32_t average;
-  int32_t computer_buf;
-  int32_t computer_now;
+  uint32_t    real_out;
+  uint32_t    average;
+  int32_t     computer_buf;
+  int32_t     computer_now;
 }output_data;
 
-cv::Mat img_counter(cv::Mat input);
-int img_get_frame(void);
+int img_counter   (cv::Mat input);
+int img_get_frame (void);
