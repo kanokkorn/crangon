3af73d60dad09ac3b088535b05dc6712522c2d4b
kanokkorn <kanokorn@outlook.jp>
2024-04-04 23:14:31 +0700

    give file a new name

--- crangc.cc
+++ crangc.cc
@@ -1,24 +1,28 @@
 /*
  * 	File: main.cc
- * 	Author: kanokkorn kornkankit <kanokorn@outlook.jp>
- * 	consider rewrite to be usable next merge
+ *  Author: kanokkorn kornkankit <kanokorn@outlook.jp>
+ * 	written on Linux, designed to work on FreeBSD 13.x and later
  */
 
-#include "main.hh"
+#include "crangc.hh"
+#include <iostream>
 
-/* error message - */
-const char* cam_error = "can't open camera\n";
-const char* config_error = "failed to load config file\n";
+/* const error message - */
+const char* cam_error = "can't open camera or doesn't existed\n";
+const char* config_error = "failed to parsing config file, make sure cranconfig is exists\n";
 const char* check_cam = "checking camera\n";
 
-/* contour and return counting number */
-cv::Mat img_counter(cv::Mat input) {
+/* cheap implement from Python version, not fine tuning yet */
+
+int img_counter(cv::Mat input) {
+  int total_unit = 0;
   cv::cvtColor(input, input, cv::COLOR_BGR2GRAY);
   cv::threshold(input, input, 128, 255, cv::THRESH_BINARY);
   std::vector<std::vector<cv::Point> > contours;
   cv::Mat contourOutput = input.clone();
-  cv::findContours( contourOutput, contours, cv::RETR_LIST, cv::CHAIN_APPROX_NONE);
+  cv::findContours(contourOutput, contours, cv::RETR_LIST, cv::CHAIN_APPROX_NONE);
   cv::Mat contourImage(input.size(), CV_8UC3, cv::Scalar(0, 0, 0));
+  
   cv::Scalar colors[3];
   colors[0] = cv::Scalar(255, 0, 0);
   colors[1] = cv::Scalar(0, 255, 0);
@@ -26,10 +30,13 @@ cv::Mat img_counter(cv::Mat input) {
   for (size_t idx = 0; idx < contours.size(); idx++) {
     cv::drawContours(contourImage, contours, idx, colors[idx % 5]);
   }
-  return contourImage;
+  return total_unit;
 }
 
-/* read frame from camera input or video */
+/*
+   read frame from camera input or video 
+   refactor planned
+*/
 int img_get_frame(void) {
   cv::VideoCapture cap(CAMERA_ID, cv::CAP_ANY);
   if (!cap.isOpened()) {
@@ -47,30 +54,33 @@ int img_get_frame(void) {
     }
     cv::imshow("normal output", frame);
     cv::imshow("process output", img_counter(frame));
+
+    /* find better way to quit program */
     char q = (char)cv::waitKey(1);
-    if (q == 27)
-      break;
+    if (q == 27) {
+      exit(EXIT_FAILURE);
+    }
   }
   cv::destroyAllWindows();
   return 0;
 }
 
-/* if got SIGINT or throw error, tell overwatch to restart again
- * reserved for actual hardware
+/* 
+ *   TODO:
+ *   - daemonise this program into `crangd`
+ *   - config to run on start up boot
+ *   - waiting client to connect to socket UDS or TCP
+ *   - 
  */
-#if defined(__aarch64__) || defined(_M_ARM64)
-void int_handler(int signum) {
-  _exit(signum);
-}
-#endif
 
 int main(int argc, char** argv) {
-  char *c;
+  // std::cout << "crangc" << GRN + VERSION + RST << "is starting up" << BLU + PLATFORM + RST<< std::endl;
+  // platform independent code
   write(1, cam_error, strlen(cam_error));
-  std::ifstream ifs("config_file");
+  std::ifstream ifs("cranconfig");
   if (ifs.fail()) {
     write(1, config_error, strlen(config_error));
-    exit(-1);
+    exit(EXIT_FAILURE);
   }
   write(1, check_cam, strlen(check_cam));
   /* read image from camera */
---
+++ crangc.hh
@@ -0,0 +1,78 @@
+/* pre-define value for makefile */
+#define VERSION   1
+#define REVISION  1
+
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <string>
+#include <fstream>
+
+/* unix pipe for gui front-end + syscall for better stdout throughput */
+#if defined(__linux__) || defined (__aarch64__)
+#define   PLATFORM "x86_64 Linux"
+#include <sys/types.h>
+#include <sys/ipc.h>
+#endif
+
+#if defined(__linux__) || defined (__aarch64__)
+#define   PLATFORM "x86_64 Linux"
+#include <sys/types.h>
+#include <sys/ipc.h>
+#endif
+
+#include <opencv4/opencv2/opencv.hpp>
+#include <opencv4/opencv2/core/core.hpp>
+#include <opencv4/opencv2/opencv_modules.hpp>
+#include <opencv4/opencv2/imgproc/imgproc.hpp>
+
+/* 
+ *  prototyping  ->  x86 & Aarch64 linux
+ *  deploying    ->  Aarch64 FreeBSD img
+ */
+#if defined(__unix__) || defined(__UNIX__) || (defined(__APPLE__) && defined(__MACH__))
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/ipc.h>
+#include <linux/limits.h>
+#include <linux/param.h>
+
+/* another opencv include for some reasons */
+#include <opencv4/opencv2/core/core.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#endif 
+
+/* enable OpenMP if platform support */ 
+#ifdef OMP
+#include <omp.h>
+#endif
+
+/* what's file ? and how valid ? */
+extern "C" {
+  int check_valid_file(char*);
+  #include "parser.c"
+};
+
+/* colour */
+#define RED "\x1B[31m"
+#define GRN "\x1B[32m"
+#define YEL "\x1B[33m"
+#define BLU "\x1B[34m"
+#define MAG "\x1B[35m"
+#define CYN "\x1B[36m"
+#define WHT "\x1B[37m"
+#define RST "\x1B[0m"
+
+/* fixed value camera setting based on webcam */
+#define BUF_SIZE    4096
+#define COLR_CHAN   3
+#define CAM_WIDTH   1280
+#define CAM_HEIGHT  720
+#define CAMERA_ID   0
+#define CAMERA_PATH "/dev/video"
+
+/* main counter */
+int img_counter   (cv::Mat input);
+int img_get_frame (void);
